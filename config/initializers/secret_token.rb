b0158ec8f2744cf12823d3277df794c045991944
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2012-09-10 16:21:33 -0400

    initial Rails skeleton commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wftb::Application.config.secret_token = '1571511f6c92689f3e269039dc5061100e4064a0c8a542b07b1c8ea66ca873cc8dc107992ed001be1e383b7a9411ab7b5cdd1a60d5a3c44bdbb514a5fa5d0009'
