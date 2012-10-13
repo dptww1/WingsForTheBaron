8d381cbe4b9fa68006695ff1803d3de79ce74615
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2012-10-13 14:32:04 -0400

    some build-out of war status card functionality

--- app/models/game.rb
+++ app/models/game.rb
@@ -42,21 +42,64 @@ class Game < ActiveRecord::Base
     nil
   end
 
+  def execute_war_status_card(card)
+    if card.do_inflation
+      self.inflation += 1 
+      # TODO reduce player's Score
+    end
+
+    self.german_morale = [self.german_morale - card.german_morale_loss, 0].max
+
+    self.allied_morale = [self.allied_morale - card.allied_morale_loss, 0].max
+
+    self.contracts_available += [card.new_contracts - self.contracts_left, 0].max
+
+    if card.do_reshuffle
+      war_status_card_draws.clear
+      war_status_card_draws << WarStatusCard.all
+    end
+
+    if card.upgraded_allied_ac && allied_power < max_player_power
+      self.allied_power = allied_power + (1..6).to_a.sample
+
+    elsif card.new_allied_ac
+      self.allied_power = self.allied_power + (1..6).to_a.sample
+
+    elsif card.allied_technology_leap
+      self.allied_power = [self.allied_power, max_player_power].max + (1..6).to_a.sample
+    end
+
+    # TODO power bonus to morale
+    # TODO game over? (including extra inflation)
+    # TODO save game state
+  end
+  
+  # Parameter is for testing purposes
+  def draw_war_status_card(card_num=nil)
+    card = card_num ? war_status_card_draws.find { |c| c.card_num == card_num } : war_status_card_draws.sample
+    war_status_card_draws.delete card
+    card
+  end
+
+  def max_player_power
+    games_players.inject(0) { |sum, player| sum += player.power }
+  end
+
   def at_least_two_players
     errors.add(:base, "You must have at least two players") unless games_players.size >= 2
   end
 
   def set_defaults
-    self.allied_morale  = 25
-    self.allied_power   = 4
-    self.complete       = false
-    # contracts_available depends on number of players
-    self.contracts_left = 0
+    self.allied_morale       = 25
+    self.allied_power        = 4
+    self.complete            = false
+    self.contracts_available = 4 # TODO: should depend on number of players
+    self.contracts_left      = 0
     # creator is filled in by controller
-    self.games_players  = []
-    self.german_morale  = 25
-    self.inflation      = 0
-    self.turn           = 1
+    self.games_players       = []
+    self.german_morale       = 25
+    self.inflation           = 0
+    self.turn                = 1
 
     self.war_status_card_draws << WarStatusCard.all
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,23 +1,84 @@
 require "spec_helper"
 
-describe "Games" do
+describe Game do
   before(:each) do
-    @g = Game.new
-  end
-  
-  describe "New game" do
-    it "should have initialized attributes" do
-      @g.allied_morale.should  == 25
-      @g.allied_power.should   ==  4
-      @g.complete.should       == false
-      @g.german_morale.should  == 25
-      @g.inflation.should      == 0
-      @g.turn.should           == 1
-    end
+    user1 = User.create!(email: "rspec1@example.com", password: "rspec1@ex", password_confirmation: "rspec1@ex")
+    user2 = User.create!(email: "rspec2@example.com", password: "rspec1@ex", password_confirmation: "rspec1@ex")
+    user3 = User.create!(email: "rspec3@example.com", password: "rspec1@ex", password_confirmation: "rspec1@ex")
 
-    it "should have 16 war status cards in the draw pile" do
-      @g.war_status_card_draws.size.should == 16
-    end
+    @game = Game.create(name: "RSpec Game", creator: user2)
+    @game.games_players << GamesPlayer.create!(user: user1, side_name: "albatros")
+    @game.games_players << GamesPlayer.create!(user: user2, side_name: "fokker")
+    @game.games_players << GamesPlayer.create!(user: user3, side_name: "pfalz")
+
+    @game.save!
   end
 
+  it "has initialized attributes" do
+    @game.allied_morale.should  == 25
+    @game.allied_power.should   ==  4
+    @game.complete.should       == false
+    @game.german_morale.should  == 25
+    @game.inflation.should      == 0
+    @game.turn.should           == 1
+    end
+
+  it "should have 16 war status cards in the draw pile" do
+    @game.war_status_card_draws.size.should == 16
+  end
+
+  describe "#draw_war_status_card" do
+    it "removes a card from the War Status deck" do
+      num_cards = @game.war_status_card_draws.size
+      card = @game.draw_war_status_card
+      card.should_not be_nil
+      @game.war_status_card_draws.size().should == num_cards - 1
+    end
+  end
+
+  describe "#execute_war_status_card" do
+    it "lowers the morale of both sides" do
+      card = @game.draw_war_status_card
+      card.upgraded_allied_ac = true
+      card.new_allied_ac = true
+      card.allied_technology_leap = true
+      @game.execute_war_status_card(card)
+
+      @game.allied_morale.should be < 25
+      @game.german_morale.should be < 25
+    end
+
+    it "adds inflation when required" do
+      @game.execute_war_status_card(WarStatusCard.where(card_num: 1).first)  # no inflation
+      @game.inflation.should == 0
+
+      @game.execute_war_status_card(WarStatusCard.where(card_num: 7).first)  # inflation
+      @game.inflation.should == 1
+    end
+
+    it "adds new contracts, less this turn's unawarded contracts" do
+      card = WarStatusCard.where(card_num: 3).first   # new_contracts == 3
+      @game.contracts_available = 42
+
+      @game.contracts_left = 0  # all contracts awarded
+      @game.execute_war_status_card(card)
+      @game.contracts_available.should == 45
+
+      @game.contracts_left = 1  # so 3 new contracts, less 1 remaining == 2
+      @game.execute_war_status_card(card)
+      @game.contracts_available.should == 47 # 45 + 2
+
+      @game.contracts_left = 3  # 3 new contracts less 3 remaining == 0
+      @game.execute_war_status_card(card)
+      @game.contracts_available.should == 47 # 47 + 0
+    end
+
+    it "reshuffles when required" do
+      @game.execute_war_status_card(@game.draw_war_status_card(3))   # no reshuffle
+      @game.war_status_card_draws.size.should == 15   # full deck of sixteen, less one
+
+      @game.execute_war_status_card(@game.draw_war_status_card(1))  # reshuffle
+      @game.war_status_card_draws.size.should == 16   # all cards back in deck
+    end
+  end
 end
